d1e0281d6fb8c033b425399f7d09e137808c4004
Tec4Gen <steam.com77@yandex.ru>
2020-02-19 18:14:21 +0400

    lolez

---
+++ 13(I)/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Example;
+//13 (I) количество узлов, значение которых меньше среднего арифметического;
+class Main_
+{
+    static void Main()
+    {
+        BinaryTree tree = new BinaryTree(); //инициализируем дерево
+                                            //на основе данных файла создаем дерево
+        using (StreamReader fileIn = new StreamReader("../../../input.txt"))
+        {
+            string line = fileIn.ReadToEnd();
+            string[] data = line.Split(' ');
+            try
+            {
+                foreach (string item in data)
+                {
+
+                    tree.Add(int.Parse(item));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Файл пустой");
+            }
+            
+        }
+        tree.FindNode(); //используя прямой обход выводим на экран узлы дерева
+    }
+}
